38e3977892146873898a9bdbb6d9fde0ac89d899
yanagitani <takanori.yanagitani@gmail.com>
2026-06-26 07:48:28 +0900

    chore: wasi related shells

---
+++ wasm.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+iname=./sigmoid.wat
+oname=./sigmoid.wasm
+
+wat2wasm \
+	"${iname}" \
+	-o "${oname}" \
+	--enable-relaxed-simd \
+	--enable-tail-call || exec sh -c '
+		echo unable to compile.
+		exit 1
+	'
+
+ls -l "${oname}"
